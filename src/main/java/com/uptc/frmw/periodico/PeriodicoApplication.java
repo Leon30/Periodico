172cc6c11d4478d4a2595bcbda6f8bbf63f90045
leonStudynow <leon@studynow.education>
2024-05-13 15:11:21 -0500

    init

---
+++ src/main/java/com/uptc/frmw/periodico/PeriodicoApplication.java
@@ -0,0 +1,13 @@
+package com.uptc.frmw.periodico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeriodicoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeriodicoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uptc/frmw/periodico/PeriodicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uptc.frmw.periodico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeriodicoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
